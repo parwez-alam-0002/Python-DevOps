dcb2996ed3855d667a36fd780bc413737f512183
Shaikh Parwez Alam <parwez.alam.0618@gmail.com>
2025-11-02 14:04:27 +0530

    rmv dir

---
+++ Arithmetic.py
@@ -0,0 +1,11 @@
+a=10
+b=2
+
+print(f'Addition {a+b}') 
+print(f'Substraction {b-a}') 
+print(f'Multiplication {a*b}') 
+print(f'Division {a/b}') 
+print(f'Floor Division {a//b}') 
+print(f'Exponential {a**b}') 
+print(f'Right Shift {a>>b}') 
+print(f'Left Shift {a<<b}') 
---
+++ python-intro.py
@@ -0,0 +1,7 @@
+print('Hello World')
+
+a,b,c=1,2,3
+print(f'a : {a}, b : {b} c : {c}')
+
+a=b=c=10
+print(f'a : {a}, b : {b} c : {c}')
