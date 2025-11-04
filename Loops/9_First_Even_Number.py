5f17a1e90137968edbcb3294722806cec42cae44
Shaikh Parwez Alam <parwez.alam.0618@gmail.com>
2025-11-04 23:59:18 +0530

    Create 9_First_Even_Number.py

---
+++ Loops/9_First_Even_Number.py
@@ -0,0 +1,5 @@
+# Problem 2: Find first even number
+# This program goes through user inputs and stops as soon as it finds the first even number.
+
+while not (num:=(int(input("Enter the Number: ")))%2==0):
+        print("Enter Even number to stop this.")
