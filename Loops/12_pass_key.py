3d7798587702665a8461a0a70db0042f62d7e895
Shaikh Parwez Alam <parwez.alam.0618@gmail.com>
2025-11-05 00:23:09 +0530

    Create 12_pass_key.py

---
+++ Loops/12_pass_key.py
@@ -0,0 +1,5 @@
+# Problem 1: Use pass for future implementation
+#  Loop through a list of tasks and use pass where logic is yet to be added, keeping the loop valid.
+
+for i in range(1,10):
+    pass
