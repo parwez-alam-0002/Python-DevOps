1f66e5b193423a32e2028d8d900dacae80421b26
Shaikh Parwez Alam <parwez.alam.0618@gmail.com>
2025-11-05 23:13:31 +0530

    Create 3_Keyword_Arguments.py

---
+++ Functions/3_Keyword_Arguments.py
@@ -0,0 +1,8 @@
+#Keyword Arguments
+
+def keyword_Function(fname,lname):
+    print(fname+" "+lname)
+
+keyword_Function(lname="Parwez",fname="Shaikh")
+keyword_Function(fname='Parwez',lname='Alam')
+#keyword_Function(lname='Parwez','Alam')
