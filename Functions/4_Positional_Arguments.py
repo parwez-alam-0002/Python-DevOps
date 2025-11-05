27941be8ff316f453c66772020c4bca1911385ff
Shaikh Parwez Alam <parwez.alam.0618@gmail.com>
2025-11-05 23:15:18 +0530

    Create 4_Positional_Arguments.py

---
+++ Functions/4_Positional_Arguments.py
@@ -0,0 +1,10 @@
+#3. Positional Arguments
+def nameAge(name, age):
+    print("Hi, I am", name)
+    print("My age is ", age)
+
+print("Case-1:")
+nameAge("Suraj", 27)
+
+print("\nCase-2:")
+nameAge(27, "Suraj")
