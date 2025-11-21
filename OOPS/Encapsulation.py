fe70052930127ed5bdeefb35ec100c14a13fdd91
Shaikh Parwez Alam <parwez.alam.0618@gmail.com>
2025-11-22 01:30:45 +0530

    Create Encapsulation.py

---
+++ OOPS/Encapsulation.py
@@ -0,0 +1,27 @@
+#Encapsulation
+class Bank:
+    def __init__(self,balance,eid):
+        self.balance=balance
+        self.eid=eid
+    
+    #declaring protected method
+    def _show_balance(self):
+        print(f'Account balance is {self.balance}.')
+        
+    #declaring private method
+    def __depositeBalance(self,amount):
+        self.balance+=amount
+       
+    #public method 
+    def checkDetails(self,eid,amt):
+        if self.eid==eid and amt>-1:
+            self.__depositeBalance(amt)
+            self._show_balance()
+        else:
+            print(f'Enter the correct details')
+            
+person=Bank(10000,101)
+person.checkDetails(101,1000)
+
+        
+        
